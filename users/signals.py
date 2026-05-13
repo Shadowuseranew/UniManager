4f6956a85c04baaab812a39583dac632b6aaa093
shadowking <naruto1shippuden01@gmail.com>
2026-05-13 05:50:18 +0500

    Teacher profil uchun signal qo'shildi

--- users/signals.py
+++ users/signals.py
@@ -1,21 +1,27 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User
+from django.db import transaction
+from .models import User, TeacherProfile
 from academy.models import Student
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    # Agar yangi foydalanuvchi yaratilgan bo'lsa (created=True)
-    # va uning roli talaba bo'lsa
-    if created and instance.role == 'student':
-        Student.objects.create(
-            user=instance,
-            student_id=f"ST-{instance.username}", # Avtomatik ID berish
-            phone_number=instance.phone_number or ''
-        )
+def create_user_profiles(sender, instance, created, **kwargs):
+    if created:
+        if instance.role == 'student':
+            Student.objects.create(
+                user=instance,
+                student_id=f"ST-{instance.username}",
+                phone_number=instance.phone_number or ''
+            )
+        elif instance.role == 'teacher':
+            TeacherProfile.objects.create(
+                user=instance,
+                specialization=''
+            )
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    # Foydalanuvchi ma'lumotlari yangilanganda profilni ham yangilash
+def save_user_profiles(sender, instance, **kwargs):
     if instance.role == 'student' and hasattr(instance, 'student_profile'):
         instance.student_profile.save()
+    elif instance.role == 'teacher' and hasattr(instance, 'teacher_profile'):
+        instance.teacher_profile.save()
